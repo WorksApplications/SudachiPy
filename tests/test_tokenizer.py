164d0be54fe440c177d978103244ee0188cc7169
izziiyt <ahne.ahne.ahne@gmail.com>
2019-06-12 20:39:06 +0900

    test: add tests to tokenizer.py

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,7 +1,7 @@
 import json
 import unittest
 
-from sudachipy import config, dictionary
+from sudachipy import config, dictionary, tokenizer
 
 
 class TestTokenizer(unittest.TestCase):
@@ -42,6 +42,33 @@ class TestTokenizer(unittest.TestCase):
 
         ms = self.tokenizer_obj.tokenize(None, '京')
         self.assertEqual(1, len(ms))
+
+    def test_multi_granular(self):
+        text = '医薬品安全管理責任者'
+        mode = tokenizer.Tokenizer.SplitMode.C
+        tokenized = [m.surface() for m in self.tokenizer_obj.tokenize(mode, text)]
+        self.assertEqual(['医薬品', '安全', '管理責任者'], tokenized)
+        mode = tokenizer.Tokenizer.SplitMode.B
+        tokenized = [m.surface() for m in self.tokenizer_obj.tokenize(mode, text)]
+        self.assertEqual(['医薬品', '安全', '管理', '責任者'], tokenized)
+        mode = tokenizer.Tokenizer.SplitMode.A
+        tokenized = [m.surface() for m in self.tokenizer_obj.tokenize(mode, text)]
+        self.assertEqual(['医薬', '品', '安全', '管理', '責任', '者'], tokenized)
+
+    def test_morpheme(self):
+        mode = tokenizer.Tokenizer.SplitMode.A
+        m = self.tokenizer_obj.tokenize(mode, '食べ')[0]
+        self.assertEqual('食べ', m.surface())
+        self.assertEqual('食べる', m.dictionary_form())
+        self.assertEqual('タベ', m.reading_form())
+        self.assertEqual(['動詞', '一般', '*', '*', '下一段-バ行', '連用形-一般'], m.part_of_speech())
+
+    def test_normalize(self):
+        mode = tokenizer.Tokenizer.SplitMode.A
+        m = self.tokenizer_obj.tokenize(mode, '食べ')[0]
+        self.assertEqual('付属', self.tokenizer_obj.tokenize(mode, '附属')[0].normalized_form())
+        self.assertEqual('サマー', self.tokenizer_obj.tokenize(mode, "SUMMER")[0].normalized_form())
+        self.assertEqual('シミュレーション', self.tokenizer_obj.tokenize(mode, "シュミレーション")[0].normalized_form())
 
 
 if __name__ == '__main__':
