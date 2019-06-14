2f0eefc27f6aba3978588db4d1683791e2ff4060
izziiyt <ahne.ahne.ahne@gmail.com>
2019-06-14 14:48:40 +0900

    test: add test templates

---
+++ tests/plugin/test_join_numeric_plugin.py
@@ -0,0 +1,8 @@
+import unittest
+
+
+class TestJoinKatakanaOOVPlugin(unittest.TestCase):
+    # Implementation should be after issue #7
+    # see https://github.com/WorksApplications/SudachiPy/issues/7
+    def setUp(self):
+        pass
