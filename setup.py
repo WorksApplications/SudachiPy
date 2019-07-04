a0e3a1b74abe7fc37d58ad4e9257bf02b81119d4
hiroshi-matsuda-rit <40782025+hiroshi-matsuda-rit@users.noreply.github.com>
2019-07-04 18:39:32 +0900

    bump up build version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="SudachiPy",
-      version="0.1.0",
+      version="0.1.2",
       description="Python version of Sudachi, the Japanese Morphological Analyzer",
       long_description="",
       url="https://github.com/WorksApplications/Sudachi",
