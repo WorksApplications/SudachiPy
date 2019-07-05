9f818520ef0f8e12e4c504fae877372a0d46fc3c
Kazuma TAKAOKA <takaoka_k@worksap.co.jp>
2019-07-05 17:17:17 +0900

    ci: change the version for test

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="SudachiPy",
-      version="0.2.0",
+      version="0.2.0-test",
       description="Python version of Sudachi, the Japanese Morphological Analyzer",
       long_description=open('README.md').read(),
       long_description_content_type="text/markdown",
