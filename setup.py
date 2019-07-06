c5cdc69c8c857de4fcfa5f3d8611f7a5fab486c7
hiroshi <hiroshi_matsuda@megagon.ai>
2019-07-06 19:56:00 +0900

    move resources under sudachipy/

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(name="SudachiPy",
       author="Works Applications",
       author_email="takaoka_k@worksap.co.jp",
       packages=find_packages(include=["sudachipy", "sudachipy.*"]),
+      package_data={"": ["resources/*.json", "resources/*.dic", "resources/*.def"]},
       entry_points={
           "console_scripts": ["sudachipy=sudachipy.command_line:main"],
       },
       install_requires=["sortedcontainers>=2.1.0,<2.2.0"],
-      include_package_data=True,
       )
