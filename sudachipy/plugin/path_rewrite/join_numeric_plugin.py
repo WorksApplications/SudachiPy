8fc504d7f612f5b554adb5bc51e68664ad5b7901
Yuto Ichikawa <ahne.ahne.ahne@gmail.com>
2019-07-19 00:37:38 +0900

    feat: add test_join_numeric_plugin.py

--- sudachipy/plugin/path_rewrite/join_numeric_plugin.py
+++ sudachipy/plugin/path_rewrite/join_numeric_plugin.py
@@ -7,13 +7,16 @@ from .path_rewrite_plugin import PathRewritePlugin
 class JoinNumericPlugin(PathRewritePlugin):
 
     _numeric_pos_id = None
+    _enable_normalize = True
 
     def __init__(self, json_obj):
+        self._NUMERIC_POS = ['名詞', '数詞', '*', '*', '*', '*']
+        if not json_obj:
+            return
         if 'joinKanjiNumeric' in json_obj:
             pass  # Todo warn
         if 'enableNormalize' in json_obj:
             self._enable_normalize = json_obj['enableNormalize']
-        self._NUMERIC_POS = ['名詞', '数詞', '*', '*', '*', '*']
 
     def set_up(self, grammar):
         self._numeric_pos_id = grammar.get_part_of_speech_id(self._NUMERIC_POS)
@@ -23,8 +26,8 @@ class JoinNumericPlugin(PathRewritePlugin):
         comma_as_digit = True
         period_as_digit = True
         parser = NumericParser()
-
         i = -1
+
         while i < len(path) - 1:
             i += 1
             node = path[i]
@@ -37,44 +40,51 @@ class JoinNumericPlugin(PathRewritePlugin):
                     parser.clear()
                     begin_index = i
 
-                for j in range(len(s)):
-                    c = s[j]
+                for c in s:
                     if not parser.append(c):
                         if begin_index >= 0:
-                            comma_as_digit = parser.error_state != NumericParser.Error.COMMA
-                            period_as_digit = parser.error_state != NumericParser.Error.POINT
-                            i = begin_index - 1
+                            if parser.error_state == NumericParser.Error.COMMA:
+                                comma_as_digit = False
+                                i = begin_index - 1
+                            elif parser.error_state == NumericParser.Error.POINT:
+                                period_as_digit = False
+                                i = begin_index - 1
                             begin_index = -1
                         break
-            else:
-                if begin_index >= 0:
-                    if parser.done():
-                        self._concat(path, begin_index, len(path), lattice, parser)
-                    else:
-                        ss = path[-1].get_word_info().normalized_form
-                        if parser.error_state == NumericParser.Error.COMMA and ss == ',' or \
-                           parser.error_state == NumericParser.Error.POINT and ss == '.':
-                            self._concat(path, begin_index, len(path) - 1, lattice, parser)
-                            i = begin_index + 2
-                begin_index = -1
-                comma_as_digit = not comma_as_digit and s != ','
-                period_as_digit = not period_as_digit and s != '.'
+                continue
 
             if begin_index >= 0:
                 if parser.done():
-                    self._concat(path, begin_index, len(path), lattice, parser)
+                    self._concat(path, begin_index, i, lattice, parser)
+                    i = begin_index + 1
                 else:
-                    ss = path[-1].get_word_info().normalized_form
-                    if parser.error_state == NumericParser.Error.COMMA and ss == ',' or \
-                       parser.error_state == NumericParser.Error.POINT and ss == '.':
-                        self._concat(path, begin_index, len(path) - 1, lattice, parser)
+                    ss = path[i - 1].get_word_info().normalized_form
+                    if (parser.error_state == NumericParser.Error.COMMA and ss == ',') or \
+                       (parser.error_state == NumericParser.Error.POINT and ss == '.'):
+                        self._concat(path, begin_index, i - 1, lattice, parser)
+                        i = begin_index + 2
+            begin_index = -1
+            if not comma_as_digit and s != ',':
+                comma_as_digit = True
+            if not period_as_digit and s != '.':
+                period_as_digit = True
+
+        if begin_index >= 0:
+            if parser.done():
+                self._concat(path, begin_index, len(path), lattice, parser)
+            else:
+                ss = path[-1].get_word_info().normalized_form
+                if (parser.error_state == NumericParser.Error.COMMA and ss == ',') or \
+                   (parser.error_state == NumericParser.Error.POINT and ss == '.'):
+                    self._concat(path, begin_index, len(path) - 1, lattice, parser)
 
     def _concat(self, path, begin, end, lattice, parser) -> None:
         if path[begin].get_word_info().pos_id != self._numeric_pos_id:
             return
         if self._enable_normalize:
             normalized_form = parser.get_normalized()
-            if end - begin < 1 or not normalized_form == path[begin].get_word_info.normalized_form:
+            if end - begin > 1 or normalized_form != path[begin].get_word_info().normalized_form:
                 self.concatenate(path, begin, end, lattice, normalized_form)
-            elif end - begin > 1:
-                self.concatenate(path, begin, end, lattice, None)
+            return
+        if end - begin > 1:
+            self.concatenate(path, begin, end, lattice, '')
--- tests/plugin/test_join_numeric_plugin.py
+++ tests/plugin/test_join_numeric_plugin.py
@@ -1,42 +1,137 @@
+import os
 import unittest
-# import os
-# from sudachipy.dictionary import Dictionary
-# from sudachipy.plugin.path_rewrite import JoinNumericPlugin
-# from sudachipy.utf8inputtextbuilder import UTF8InputTextBuilder
+
+from sudachipy.dictionary import Dictionary
+from sudachipy.plugin.path_rewrite import JoinNumericPlugin
+from sudachipy.utf8inputtextbuilder import UTF8InputTextBuilder
 
 
 class TestJoinNumericOOVPlugin(unittest.TestCase):
-    """
-    Implementation of JoinNumericOOVPlugin is out of date,
-    NumericParser required beforehand
-    """
 
     def setUp(self):
         pass
-    #     resource_dir = os.path.join(os.pardir, 'resources')
-    #     self.dict_ = Dictionary(os.path.join(resource_dir, 'sudachi.json'), resource_dir)
-    #     self.tokenizer = self.dict_.create()
-    #     self.plugin = JoinNumericPlugin(None)
-    #     self.plugin.set_up(self.dict_.grammar)
-    #
-    # def test_digit(self):
-    #     path = self.get_path('123円20銭')
-    #     self.assertEqual(4, len(path))
-    #     self.assertEqual('123', path[0].get_word_info().surface)
-    #     self.assertEqual('20', path[2].get_word_info().surface)
-    #
-    #     path = self.get_path('080-121')
-    #     self.assertEqual(3, len(path))
-    #     self.assertEqual('080', path[0].get_word_info().surface)
-    #     self.assertEqual('121', path[1].get_word_info().surface)
-    #
-    # def get_path(self, text: str):
-    #     input_ = UTF8InputTextBuilder(text, self.tokenizer.grammar).build()
-    #     self.tokenizer.build_lattice(input_)
-    #     path = self.tokenizer.lattice.get_best_path()
-    #     self.plugin.rewrite(input_, path, self.tokenizer.lattice)
-    #     self.tokenizer.lattice.clear()
-    #     return path
+        resource_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, 'resources')
+        self.dict_ = Dictionary(os.path.join(resource_dir, 'numeric_sudachi.json'), resource_dir)
+        self.tokenizer = self.dict_.create()
+        self.plugin = JoinNumericPlugin(None)
+        self.plugin.set_up(self.dict_.grammar)
+
+    def test_digit(self):
+        path = self.get_path('123円20銭')
+        self.assertEqual(4, len(path))
+        self.assertEqual('123', path[0].get_word_info().surface)
+        self.assertEqual('20', path[2].get_word_info().surface)
+
+        path = self.get_path('080-121')
+        self.assertEqual(3, len(path))
+        self.assertEqual('080', path[0].get_word_info().surface)
+        self.assertEqual('121', path[2].get_word_info().surface)
+
+    def test_kanji_numeric(self):
+        path = self.get_path('一二三万二千円')
+        self.assertEqual(2, len(path))
+        self.assertEqual('一二三万二千', path[0].get_word_info().surface)
+
+        path = self.get_path('二百百')
+        self.assertEqual(3, len(path))
+
+    def test_normalize(self):
+        self.plugin._enable_normalize = True
+        path = self.get_path('一二三万二千円')
+        self.assertEqual(2, len(path))
+        self.assertEqual('1232000', path[0].get_word_info().normalized_form)
+
+    def test_normalized_with_not_numeric(self):
+        self.plugin._enable_normalize = True
+        path = self.get_path('六三四')
+        self.assertEqual(1, len(path))
+        self.assertEqual('六三四', path[0].get_word_info().normalized_form)
+
+    def test_point(self):
+        self.plugin._enable_normalize = True
+
+        path = self.get_path('1.002')
+        self.assertEqual(1, len(path))
+        self.assertEqual('1.002', path[0].get_word_info().normalized_form)
+
+        path = self.get_path('.002')
+        self.assertEqual(2, len(path))
+        self.assertEqual('.', path[0].get_word_info().normalized_form)
+        self.assertEqual('002', path[1].get_word_info().normalized_form)
+
+        path = self.get_path('22.')
+        self.assertEqual(2, len(path))
+        self.assertEqual('22', path[0].get_word_info().normalized_form)
+        self.assertEqual('.', path[1].get_word_info().normalized_form)
+
+        path = self.get_path('22.節')
+        self.assertEqual(3, len(path))
+        self.assertEqual('22', path[0].get_word_info().normalized_form)
+        self.assertEqual('.', path[1].get_word_info().normalized_form)
+
+        path = self.get_path('.c')
+        self.assertEqual(2, len(path))
+        self.assertEqual('.', path[0].get_word_info().normalized_form)
+
+        path = self.get_path('1.20.3')
+        self.assertEqual(5, len(path))
+        self.assertEqual('20', path[2].get_word_info().normalized_form)
+
+        path = self.get_path('652...')
+        self.assertEqual(4, len(path))
+        self.assertEqual('652', path[0].get_word_info().normalized_form)
+
+    def test_comma(self):
+        self.plugin._enable_normalize = True
+
+        path = self.get_path('2,000,000')
+        self.assertEqual(1, len(path))
+        self.assertEqual('2000000', path[0].get_word_info().normalized_form)
+
+        path = self.get_path('2,00,000,000円')
+        self.assertEqual(8, len(path))
+        self.assertEqual('2', path[0].get_word_info().normalized_form)
+        self.assertEqual(',', path[1].get_word_info().normalized_form)
+        self.assertEqual('00', path[2].get_word_info().normalized_form)
+        self.assertEqual(',', path[3].get_word_info().normalized_form)
+        self.assertEqual('000', path[4].get_word_info().normalized_form)
+        self.assertEqual(',', path[5].get_word_info().normalized_form)
+        self.assertEqual('000', path[6].get_word_info().normalized_form)
+
+        path = self.get_path(',')
+        self.assertEqual(1, len(path))
+
+        path = self.get_path('652,,,')
+        self.assertEqual(4, len(path))
+        self.assertEqual('652', path[0].get_word_info().normalized_form)
+
+        path = self.get_path('256,5.50389')
+        self.assertEqual(3, len(path))
+        self.assertEqual('256', path[0].get_word_info().normalized_form)
+        self.assertEqual('5.50389', path[2].get_word_info().normalized_form)
+
+        path = self.get_path('256,550.389')
+        self.assertEqual(1, len(path))
+        self.assertEqual('256550.389', path[0].get_word_info().normalized_form)
+
+    def test_single_node(self):
+        self.plugin._enable_normalize = False
+        path = self.get_path('猫三匹')
+        self.assertEqual(3, len(path))
+        self.assertEqual('三', path[1].get_word_info().normalized_form)
+
+        self.plugin._enable_normalize = True
+        path = self.get_path('猫三匹')
+        self.assertEqual(3, len(path))
+        self.assertEqual('3', path[1].get_word_info().normalized_form)
+
+    def get_path(self, text: str):
+        input_ = UTF8InputTextBuilder(text, self.tokenizer.grammar).build()
+        self.tokenizer.build_lattice(input_)
+        path = self.tokenizer.lattice.get_best_path()
+        self.plugin.rewrite(input_, path, self.tokenizer.lattice)
+        self.tokenizer.lattice.clear()
+        return path
 
 
 if __name__ == '__main__':
