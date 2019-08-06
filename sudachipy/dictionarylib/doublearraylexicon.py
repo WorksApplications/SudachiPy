63f78b31bb53ca2c4d930f600cb830d5f6484975
Yuto Ichikawa <ahne.ahne.ahne@gmail.com>
2019-08-06 22:42:41 +0900

    refactor: modify wordparameterlist.py

--- sudachipy/dictionarylib/doublearraylexicon.py
+++ sudachipy/dictionarylib/doublearraylexicon.py
@@ -51,6 +51,9 @@ class DoubleArrayLexicon(Lexicon):
 
         self.word_infos = wordinfolist.WordInfoList(bytes_, offset, self.word_params.get_size())
 
+    def __del__(self):
+        del self.word_params
+
     def lookup(self, text: bytes, offset: int) -> Lexicon.Itr:
         key = text[offset:]
         result = self.trie.common_prefix_search(key, length=len(key))
