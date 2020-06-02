9071ca8bc50e19c33586da2f626e559064f183da
Paul O'Leary McCann <polm@dampfkraft.com>
2020-06-02 23:45:44 +0900

    Add lru_cache on get_word_info

--- sudachipy/dictionarylib/lexiconset.py
+++ sudachipy/dictionarylib/lexiconset.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from typing import List
+from functools import lru_cache
 
 from .lexicon import Lexicon
 
@@ -57,6 +58,7 @@ class LexiconSet(Lexicon):
         return self.lexicons[self.get_dictionary_id(word_id)]\
             .get_cost(self.get_word_id1(word_id))
 
+    @lru_cache
     def get_word_info(self, word_id: int) -> 'WordInfo':  # noqa: F821
         dic_id = self.get_dictionary_id(word_id)
         winfo = self.lexicons[dic_id].get_word_info(self.get_word_id1(word_id))
