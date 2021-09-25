06f317cc7fc26c232795ea7de8a80fdc4345215c
takaoka_k <takaoka_k@worksap.co.jp>
2021-09-25 18:19:27 +0900

    Build test dictionaries on the fly

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,29 +5,20 @@
 # You need to prepare system.dic in resources and tests/resources
 # see README
 
-cd $(dirname $0)
+set -e
 
-# check system.dic
-if [[ ! -f "../tests/resources/system.dic" ]]; then
-    cp ../.travis/system.dic.test ../tests/resources/system.dic
-fi
-DIFF=$(diff ../.travis/system.dic.test ../tests/resources/system.dic)
-if [[ "$DIFF" != "" ]]; then
-    cp ../.travis/system.dic.test ../tests/resources/system.dic
-fi
+# build dictionaries
+python -m sudachipy.command_line build -o tests/resources/system.dic -d "the system dictionary for the unit tests" -m tests/resources/dict/matrix.def tests/resources/dict/lex.csv
+python -m sudachipy.command_line ubuild -o tests/resources/user.dic -s tests/resources/system.dic tests/resources/dict/user.csv
 
-# check user.dic
-if [[ ! -f "../tests/resources/user.dic" ]]; then
-    cp ../.travis/user.dic.test ../tests/resources/user.dic
-fi
-DIFF=$(diff ../.travis/user.dic.test ../tests/resources/user.dic)
-if [[ "$DIFF" != "" ]]; then
-    cp ../.travis/user.dic.test ../tests/resources/user.dic
-fi
+set +e
 
 # unittest
-RES=`cd ..; python -m unittest discover tests -p '*test*.py' 2>&1`
+RES=`python -m unittest discover tests -p '*test*.py' 2>&1`
+STATUS=$?
 RES_TAIL=`echo "$RES" | tail -1`
 if [[ $RES_TAIL != "OK" ]]; then
     >&2 echo "$RES"
 fi
+
+exit $STATUS
